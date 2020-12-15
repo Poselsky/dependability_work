7a877f988cab0ab094223a03884c6c77e4e87509
Poselsky <oleg.musijenko@seznam.cz>
2020-12-15 09:35:45 +0100

    do it plz

--- index.php
+++ index.php
@@ -5,15 +5,7 @@ require_once('./classes/BlockComparer.php');
 require_once('./helpers/comb.php');
 
 $syndrome_table_X = SyndromeTable::generate_possible_points_of_failure();
-
 $syndrome_table_rand = RandomSyndromeGenerator::generate_random_failure($syndrome_table_X);
-
-$row = $syndrome_table_rand[rand(0, 9)];
-
-$something = new BlockComparer($syndrome_table_X, $syndrome_table_rand);
-
-$something->compare_columns()->pretty_print();
-$something->compare_rows()->pretty_print();
 
 echo "<link rel=\"stylesheet\" type=\"text/css\" href=\"style.css\"/>";
 // echo "-----------------------------------------------------------------";
@@ -41,9 +33,9 @@ function get_random_index_by_propability($input = [[]], $probability = [50,50]){
 }
 
 $randomRowIndex = get_random_index_by_propability($input, $prop);
-$randomRow = $syndrome_table_X[$randomRowIndex];
+$randomRow = $syndrome_table_rand[$randomRowIndex];
 
-echo $randomRowIndex;
+echo "Index of generated syndrom: " . $randomRowIndex;
 echo "<br>";
 // doplneni nahodne 0 / 1 za X v radku
 for ($i=0; $i < sizeof($randomRow); $i++) {
@@ -55,64 +47,62 @@ for ($i=0; $i < sizeof($randomRow); $i++) {
 print_r ($randomRow);
 
 $something = new BlockComparer($syndrome_table_X, $syndrome_table_rand);
-
 $something->commonRandomSyndromArray = $randomRow;
 
 $something->compare_columns()->pretty_print();
-echo $something->compare_columns()->numberOfCompares;
+
+$something->compare_rows()->pretty_print();
 
 // db connection
 
-$servername = "localhost";
-$username = "root";
-$password = "";
-
-try {
-  $conn = new PDO("mysql:host=$servername;dbname=dependability", $username, $password);
-  // set the PDO error mode to exception
-  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-  for ($j=0; $j < 100; $j++) {
-    // random row
-    $randomRowIndex = rand(0,9) < 6 ? rand(0, 4) : rand(5, 14);
-    $randomRow = $syndrome_table_X[$randomRowIndex];
-    $randomRowOriginal = new ArrayObject($randomRow);
-
-    $randomRowStr = '';
-    $randomRowOriginalStr = '';
-
-    // doplneni nahodne 0 / 1 za X v radku
-    for ($i=0; $i < sizeof($randomRow); $i++) {
-        $randomRowOriginalStr = $randomRowOriginalStr . $randomRow[$i];
-
-        if($randomRow[$i] === 'X'){
-            $randomRow[$i] = rand(0,1);
-        }
-
-        $randomRowStr = $randomRowStr . $randomRow[$i];
-    }
-
-    $something = new BlockComparer($syndrome_table_X, $syndrome_table_rand);
-
-    $something->commonRandomSyndromArray = $randomRow;
-
-    // echo $something->compare_columns()->numberOfCompares;
-    // echo $something->compare_columns()->numberOfCompares;
-    $iterations = $something->compare_columns()->numberOfCompares;
-    $faultyUnites = $something->compare_columns()->faultyUnites;
-
-    $sql = "INSERT INTO statistic (rowCompare, probability, syndromeOriginal, syndromeRandom, rowIndex, iterationToFaultyUnit, faultyUnits, description)
-        VALUES ('0', '60', '$randomRowOriginalStr', '$randomRowStr', '$randomRowIndex','$iterations','$faultyUnites' , '' )";
-      // use exec() because no results are returned
-   //  $conn->exec($sql);
-
-  }
-
-
-
-
-} catch(PDOException $e) {
-  echo "Connection failed: " . $e->getMessage();
-}
-
-?>
+//$servername = "localhost";
+//$username = "root";
+//$password = "";
+//
+//try {
+//  $conn = new PDO("mysql:host=$servername;dbname=dependability", $username, $password);
+//  // set the PDO error mode to exception
+//  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+//
+//  for ($j=0; $j < 100; $j++) {
+//    // random row
+//    $randomRowOriginal = new ArrayObject($randomRow);
+//
+//    $randomRowStr = '';
+//    $randomRowOriginalStr = '';
+//
+//    // doplneni nahodne 0 / 1 za X v radku
+//    for ($i=0; $i < sizeof($randomRow); $i++) {
+//        $randomRowOriginalStr = $randomRowOriginalStr . $randomRow[$i];
+//
+//        if($randomRow[$i] === 'X'){
+//            $randomRow[$i] = rand(0,1);
+//        }
+//
+//        $randomRowStr = $randomRowStr . $randomRow[$i];
+//    }
+//
+//    $something = new BlockComparer($syndrome_table_X, $syndrome_table_rand);
+//
+//    $something->commonRandomSyndromArray = $randomRow;
+//
+//    // echo $something->compare_columns()->numberOfCompares;
+//    // echo $something->compare_columns()->numberOfCompares;
+//    $iterations = $something->compare_columns()->numberOfCompares;
+//    $faultyUnites = $something->compare_columns()->faultyUnites;
+//
+//    $sql = "INSERT INTO statistic (rowCompare, probability, syndromeOriginal, syndromeRandom, rowIndex, iterationToFaultyUnit, faultyUnits, description)
+//        VALUES ('0', '60', '$randomRowOriginalStr', '$randomRowStr', '$randomRowIndex','$iterations','$faultyUnites' , '' )";
+//      // use exec() because no results are returned
+//   //  $conn->exec($sql);
+//
+//  }
+//
+//
+//
+//
+//} catch(PDOException $e) {
+//  echo "Connection failed: " . $e->getMessage();
+//}
+//
+//?>
